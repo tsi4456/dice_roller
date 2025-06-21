ee1a717c6c44d02cfc754383c371d33d68b8e203
tsi4456 <cremedefraise@gmail.com>
2025-06-21 19:46:48 +0100

    basic functionality

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import random
+import re
+
+DICE_REGEX = r"(?P<qty>\d+)d(?P<faces>\d+)"
 
 
 class DiceGroup:
-    def __init__(self, faces: int, qty: int):
-        self.faces = faces
-        self.qty = qty
+    def __init__(self, dice_string: str):
+        self.parse(dice_string)
+
+    def parse(self, dice_string: str) -> None:
+        m = re.fullmatch(DICE_REGEX, dice_string)
+        if not m:
+            raise ValueError("Invalid dice string")
+        self.qty = int(m["qty"])
+        self.faces = int(m["faces"])
+
+    def roll(self) -> list:
+        return [random.randint(1, self.faces) for _ in range(self.qty)]
+
+
+def main():
+    dg = DiceGroup("4d6")
+    print(dg.roll())
+
+
+if __name__ == "__main__":
+    main()
