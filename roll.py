a64074f9a7a6e46eae0c737ebf8b42a7b4cee0cb
tsi4456 <cremedefraise@gmail.com>
2026-06-05 16:03:30 +0100

    add rolling animation

--- roll.py
+++ roll.py
@@ -1,22 +1,24 @@
 import random
 import re
 import sys
+import time
 
 DICE_REGEX = r"(?P<qty>\d+)d(?P<faces>\d+)(?P<static>(\+|\-)\d+)?(?P<mod>(t|b)\d+)?"
+ROLL_DELAY_SECONDS = 1.5
 
 
 class DiceGroup:
     def __init__(self, dice_string: str):
         self.parse(dice_string)
-        self.results = self.roll()
         self.dropped_dice = []
+        self.results = self.roll()
 
     @property
     def total(self):
         return sum(self.results) + self.static_mods
 
     def __str__(self):
-        return f"{str(self.results)}{str(self.dropped_dice) if self.dropped_dice else ''} {'+' if self.static_mods>0 else ''}{self.static_mods if self.static_mods else ''}: {self.total}"
+        return f"{str(self.results)}{str(self.dropped_dice) if self.dropped_dice else ''} {'+' if self.static_mods > 0 else ''}{self.static_mods if self.static_mods else ''}: {self.total}"
 
     def parse(self, dice_string: str) -> None:
         m = re.fullmatch(DICE_REGEX, dice_string)
@@ -31,7 +33,17 @@ class DiceGroup:
         self.faces = int(m["faces"])
 
     def roll(self) -> list:
-        return [random.randint(1, self.faces) for _ in range(self.qty)]
+        # animation = "|/-\\"
+        idx = 0
+        while idx < ROLL_DELAY_SECONDS * 10:
+            print(
+                f"{[random.randint(1, self.faces) for _ in range(self.qty)]}", end="\r"
+            )
+            # print(animation[idx % len(animation)], end="\r")
+            idx += 1
+            time.sleep(0.1)
+        self.results = [random.randint(1, self.faces) for _ in range(self.qty)]
+        print(self)
 
 
 def main():
@@ -41,7 +53,7 @@ def main():
             groups = [DiceGroup(a) for a in args[1:]]
         except ValueError as e:
             print(e)
-        print(f"{'\n'.join(str(g) for g in groups)}")
+        # print(f"{'\n'.join(str(g) for g in groups)}")
     else:
         while True:
             prompt = input("> ")
